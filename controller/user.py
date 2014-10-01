ddc1d03b321e05625a0044dba7f935c61c5feb80
JP Verkamp <me@jverkamp.com>
2014-09-30 19:23:09 -0700

    Add method to create new users

--- controller/user.py
+++ controller/user.py
@@ -7,23 +7,36 @@ import model.user
 
 def register(app):
 
-    @app.route('/api/v1/user/login', methods = ["POST"])
-    def user_login():
-        '''Log a user in'''
+    @app.route('/api/v1/user', methods = ["POST"])
+    def create_user():
 
+        name = flask.request.form['name']
         email = flask.request.form['email']
         password = flask.request.form['password']
 
-        app.checkPermissions = False
-        user = model.user.byEmail(app, email)
-        logged_in = False
+        # Check that the email is not currently in use
+        if model.user.byEmail(app, email):
+            flask.abort(400)
 
-        if user and user.verifyPassword(password):
-            flask.session['user_id'] = user.id
-            logged_in = True
+        new_user = model.user.User(app, name = name, email = email)
 
-        app.checkPermissions = True
-        return json.dumps(logged_in)
+        if new_user:
+            with app.unsafe():
+                new_user.setPassword(password)
+
+            return json.dumps(new_user.id)
+        else:
+            flask.abort(400)
+
+    @app.route('/api/v1/user/me', methods = ["GET"])
+    def user_me():
+        '''Return information about the current user'''
+
+        try:
+            return user(model.user.current(app).id)
+        except:
+            return json.dumps({})
+
 
     @app.route('/api/v1/user/<id>', methods = ["GET"])
     def user(id):
@@ -36,15 +49,18 @@ def register(app):
         else:
             return json.dumps({})
 
-    @app.route('/api/v1/user/me', methods = ["GET"])
-    def user_me():
-        '''Return information about the current user'''
+    @app.route('/login', methods = ["POST"])
+    def login():
+        '''Log a user in'''
 
-        try:
-            return user(model.user.current(app).id)
-        except:
-            return json.dumps({})
+        email = flask.request.form['email']
+        password = flask.request.form['password']
 
-    @app.route('/api/v1/user/register', methods = ["POST"])
-    def create_user():
-        pass
+        with app.unsafe():
+            user = model.user.byEmail(app, email)
+
+            if user and user.verifyPassword(password):
+                flask.session['user_id'] = user.id
+                return user_me()
+
+            flask.abort(400)
