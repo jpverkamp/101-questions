0704fc32c3a00e08d42948667892c79af5ed98cd
JP Verkamp <me@jverkamp.com>
2015-02-08 11:15:26 -0800

    Fix filenames, require python3

---
+++ cron.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import sys
+if sys.version_info[0] != 3:
+    print('Use python3')
+    sys.exit()
--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+if sys.version_info[0] != 3:
+    print('Use python3')
+    sys.exit()
+
 import flask
 
 app = flask.Flask('101qs')
