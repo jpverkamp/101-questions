bd56b896adb0cc5440a320814feabf7b62409a7d
JP Verkamp <me@jverkamp.com>
2015-02-08 13:47:58 -0800

    Reformat, add ability to add many emails/questions at once

--- static/main.js
+++ static/main.js
@@ -1,112 +1,137 @@
 $(function() {
-  $('.view#landing').show();
+    $('.view#landing').show();
 
-  var renderQuestionSet = function(data) {
-    $.ajax({
-      url: '/static/questionset.template.htm',
-      cache: true,
-      dataType: 'html',
-      success: function(template) {
-        var compiledTemplate = Handlebars.compile(template);
-        var html = compiledTemplate(data);
+    var renderQuestionSet = function(data) {
+        $.ajax({
+            url: '/static/questionset.template.htm',
+            cache: true,
+            dataType: 'html',
+            success: function(template) {
+                var compiledTemplate = Handlebars.compile(template);
+                var html = compiledTemplate(data);
 
-        $('.container').hide();
-        $('#questionset').html(html).show();
+                $('.container').hide();
+                $('#questionset').html(html).show();
 
-        var editableText = function(id, field) {
-          $(id + ' .item').editable({
-            title: 'Enter new ' + field,
-            type: 'text',
-            pk: 1,
-            url: '/questionset/' + data.id + '/' + field,
-            ajaxOptions: { type: 'put' },
-            params: function(params) {
-              var result = {};
-              result[field] = params.value;
-              return result;
+                var editableText = function(id, field) {
+                    $(id + ' .item').editable({
+                        title: 'Enter new ' + field,
+                        type: 'text',
+                        pk: 1,
+                        url: '/questionset/' + data.id + '/' + field,
+                        ajaxOptions: { type: 'put' },
+                        params: function(params) {
+                            var result = {};
+                            result[field] = params.value;
+                            return result;
+                        }
+                    });
+                };
+
+                editableText('#viewTitle', 'title');
+                editableText('#viewFrequency', 'frequency');
+
+                var editableList = function(id, field) {
+                    $(id + ' .new').click(function() {
+                        var postData = {};
+                        postData[field] = '';
+
+                        $.ajax({
+                            type: 'POST',
+                            url: '/questionset/' + data.id + '/' + field,
+                            data: postData,
+                            dataType: 'json',
+                            success: renderQuestionSet
+                        });
+                    });
+
+                    $(id + ' .import').click(function() {
+                        console.log('hello word');
+
+                        var postData = {};
+                        postData[field] = '';
+
+                        $('#import-dialog .field-name').text(field + 's');
+                        $('#import-dialog-values').val('')
+                        $('#import-dialog').modal('show');
+
+                        $('#import-dialog-save').unbind('click').click(function() {
+                            $('#import-dialog').modal('hide');
+
+                            var postData = {};
+                            postData[field + 's'] = JSON.stringify($('#import-dialog-values').val().split(/\n/g));
+                            $.ajax({
+                                type: 'POST',
+                                url: '/questionset/' + data.id + '/' + field + 's',
+                                data: postData,
+                                dataType: 'json',
+                                success: renderQuestionSet
+                            });
+                        });
+                    });
+
+                    $(id + ' li').each(function(i, el) {
+                        $(el).find('.delete').click(function() {
+                            $.ajax({
+                                type: 'DELETE',
+                                url: '/questionset/' + data.id + '/' + field + '/' + i,
+                                dataType: 'json',
+                                success: renderQuestionSet
+                            });
+                            return false;
+                        });
+
+                        $(el).find('.item').editable({
+                            title: 'Enter new ' + field,
+                            type: 'text',
+                            pk: 1,
+                            url: '/questionset/' + data.id + '/' + field + '/' + i,
+                            ajaxOptions: { type: 'put' },
+                            params: function(params) {
+                                var result = {};
+                                result[field] = params.value;
+                                return result;
+                            }
+                        });
+                    });
+                };
+
+                editableList('#viewEmails', 'email');
+                editableList('#viewQuestions', 'question');
             }
-          });
-        };
+        });
+    };
 
-        editableText('#viewTitle', 'title');
-        editableText('#viewFrequency', 'frequency');
+    $('#loadButton').click(function() {
+        var id = $('input[name="loadID"]').val();
 
-        var editableList = function(id, field) {
-          $(id + ' .new').click(function() {
-            var postData = {};
-            postData[field] = '';
+        $.ajax({
+            type: 'GET',
+            url: '/questionset/' + id,
+            dataType: 'json',
+            success: renderQuestionSet
+        });
 
-            $.ajax({
-              type: 'POST',
-              url: '/questionset/' + data.id + '/' + field,
-              data: postData,
-              dataType: 'json',
-              success: renderQuestionSet
-            });
-          });
-
-          $(id + ' li').each(function(i, el) {
-            $(el).find('.delete').click(function() {
-              $.ajax({
-                type: 'DELETE',
-                url: '/questionset/' + data.id + '/' + field + '/' + i,
-                dataType: 'json',
-                success: renderQuestionSet
-              });
-              return false;
-            });
-
-            $(el).find('.item').editable({
-              title: 'Enter new ' + field,
-              type: 'text',
-              pk: 1,
-              url: '/questionset/' + data.id + '/' + field + '/' + i,
-              ajaxOptions: { type: 'put' },
-              params: function(params) {
-                var result = {};
-                result[field] = params.value;
-                return result;
-              }
-            });
-          });
-        };
-
-        editableList('#viewEmails', 'email');
-        editableList('#viewQuestions', 'question');
-      }
-    });
-  };
-
-  $('#loadButton').click(function() {
-    var id = $('input[name="loadID"]').val();
-
-    $.ajax({
-      type: 'GET',
-      url: '/questionset/' + id,
-      dataType: 'json',
-      success: renderQuestionSet
+        return false;
     });
 
-    return false;
-  });
+    $('#createButton').click(function() {
+        var title = $('input[name="createTitle"]').val();
+        var frequency = $('input[name="createFrequency"]').val();
+        var email = $('input[name="createEmail"]').val();
 
-  $('#createButton').click(function() {
-    var title = $('input[name="createTitle"]').val();
-    var frequency = $('input[name="createFrequency"]').val();
-    var email = $('input[name="createEmail"]').val();
+        $.ajax({
+            type: 'POST',
+            url: '/questionset',
+            data: {
+                title: title,
+                frequency: frequency,
+                email: email
+            },
+            dataType: 'json',
+            success: renderQuestionSet
+        });
 
-    $.ajax({
-      type: 'POST',
-      url: '/questionset',
-      data: {
-        title: title,
-        frequency: frequency,
-        email: email
-      },
-      dataType: 'json',
-      success: renderQuestionSet
+        return false;
     });
-
-    return false;
-  });
 });
