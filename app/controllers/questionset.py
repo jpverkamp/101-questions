95a160329313f67a1145715ebb46921e5014b17e
JP Verkamp <me@jverkamp.com>
2015-07-15 18:56:36 -0700

    Add ability to add and edit questions on a questionset

--- app/controllers/questionset.py
+++ app/controllers/questionset.py
@@ -29,3 +29,19 @@ def register(app):
         qs = models.QuestionSet(id)
         qs['title'] = flask.request.form['value']
         return json.dumps(True)
+
+    @app.route('/questionset/<id>/questions', methods = ['POST'])
+    @lib.authenticated
+    def add_question_to_questionset(id):
+
+        qs = models.QuestionSet(id)
+        qs['questions'].append('')
+        return json.dumps(True)
+
+    @app.route('/questionset/<id>/questions/<int:index>', methods = ['POST'])
+    @lib.authenticated
+    def update_question_in_questionset(id, index):
+
+        qs = models.QuestionSet(id)
+        qs['questions'][index] = flask.request.form['value']
+        return json.dumps(True)
