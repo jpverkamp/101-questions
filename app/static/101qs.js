68786e0b93b5d10901e63ceae41640622ea20787
JP Verkamp <me@jverkamp.com>
2015-07-15 18:50:36 -0700

    Make links POSTable via the magic of JavaScript

--- app/static/101qs.js
+++ app/static/101qs.js
@@ -1,4 +1,17 @@
 $(function() {
+    $('.postit').click(function(evt) {
+        evt.preventDefault();
+
+        g = evt;
+        console.log(evt.target.href);
+
+        $.ajax({
+            type: 'POST',
+            url: evt.target.href,
+            success: function() { location.reload() }
+        });
+    });
+
     $('.editable').each(function(i, el) {
         var field = $(el).attr('data-field');
         var url = $(el).attr('data-url');
