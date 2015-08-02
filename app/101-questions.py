6a739148ed606d9eb1c88bbf406984cbe17e6558
JP Verkamp <me@jverkamp.com>
2015-08-01 19:43:48 -0700

    Force redis save on SIGTERM

--- app/101-questions.py
+++ app/101-questions.py
@@ -2,6 +2,7 @@
 
 import flask
 import os
+import signal
 import sys
 
 app = flask.Flask(__name__)
@@ -11,6 +12,13 @@ app.secret_key = '\0' * 24 # DEBUG
 
 import controllers
 controllers.register_all(app)
+
+def sigterm_handler(signo, frame):
+    print('Exiting, force Redis SAVE')
+    redis = redis.StrictRedis(host = 'redis', decode_responses = True)
+    redis.save()
+
+signal.signal(signal.SIGTERM, sigterm_handler)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8001)
